ef33c2b10576ab58c6262b83a10940ce58d2099f
Serj910vip <Sergey911vip@mail.ru>
2026-06-19 00:17:26 +0300

    change style

--- app/api/app.py
+++ app/api/app.py
@@ -1014,7 +1014,7 @@ COMMON_STYLES = """
 
     /* Стили для профиля */
     .profile-card {
-        background: var(--tg-theme-secondary-bg-color, #eb0101);
+        background: var(--tg-theme-secondary-bg-color, #eb0101) !important;
         border-radius: 16px;
         padding: 20px;
         margin-bottom: 20px;
