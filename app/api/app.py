8c3218506d8e746b6a138d5fa243adff6bcf4b18
Serj910vip <Sergey911vip@mail.ru>
2026-07-21 04:01:05 +0300

    change css

--- app/api/app.py
+++ app/api/app.py
@@ -3444,19 +3444,21 @@ async def main_app():
 
                  <!-- Меню профиля с теми же стилями, что и menu-container-home -->
 
-                <div class="profile-menu-card" onclick="window.location.href='/subscription'">
-                    <div class="left">
-                        <span class="label">Подписка</span>
-                    </div>
-                    <span class="accordion-arrow" id="arrow-wallet">
-                        <svg width="11" height="19" viewBox="0 0 11 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1.12 18.4798L1.19209e-07 17.3998L8.16 9.23984L1.19209e-07 1.07984L1.12 -0.000156403L10.36 9.23984L1.12 18.4798Z" fill="#FFFF"/>
-                        </svg>
-
-                    </span>
-                </div>
+                
                 
                 <div class="profile-menu-container">
+
+                    <div class="profile-menu-card" onclick="window.location.href='/subscription'">
+                        <div class="left">
+                            <span class="label">Подписка</span>
+                        </div>
+                        <span class="accordion-arrow" id="arrow-wallet">
+                            <svg width="11" height="19" viewBox="0 0 11 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.12 18.4798L1.19209e-07 17.3998L8.16 9.23984L1.19209e-07 1.07984L1.12 -0.000156403L10.36 9.23984L1.12 18.4798Z" fill="#FFFF"/>
+                            </svg>
+
+                        </span>
+                    </div>
                     <div class="profile-menu-card" onclick="toggleProfileMenu('wallet')">
                         <div class="left">
                             <span class="label">Кошелёк</span>
